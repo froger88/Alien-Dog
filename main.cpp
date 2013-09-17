32f38ee225f84be593d16cf9c2bcc562444ec6ed
froger88 <froger@froger.pl>
2013-09-17 22:27:32 +0200

    initail c++ project

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: mazabinski
+ *
+ * Created on 17 wrzesień 2013, 22:23
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+	return 0;
+}
+
